934ed2cabe44f1ec00af24dbe538b948bb43c694
AbhilashG12 <abhilashggg15@gmail.com>
2026-01-30 21:44:28 +0530

    initial

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import express, { Request, Response } from 'express';
+import express, { type Request,type Response } from 'express';
 
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
 
-// Define a GET route for the root URL '/'
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World from Express and TypeScript!');
 });
 
-// Start the server and listen on the specified port
+
 app.listen(PORT, () => {
   console.log(`Server is running on [http://localhost:${PORT}](http://localhost:${PORT})`);
 });
