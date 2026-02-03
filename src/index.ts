75e9c3a0df8ffb8540b4a3e7353a7368819fd3f1
AbhilashG12 <abhilashggg15@gmail.com>
2026-02-03 21:55:20 +0530

    initial

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
-import {MiniExpress} from "./miniexpress"
-import http from "http";
+// import {MiniExpress} from "./miniexpress"
+// import http from "http";
 
-const app = new MiniExpress();
+// const app = new MiniExpress();
 
-app.use((req, res, next) => {
-  console.log("Middleware hit");
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log("Middleware hit");
+//   next();
+// });
 
-app.get("/hello", (req, res) => {
-  res.end("Hello from mini express");
-});
+// app.get("/hello", (req, res) => {
+//   res.end("Hello from mini express");
+// });
 
-http.createServer((req, res) => app.handleRequest(req, res)).listen(8000);
+// http.createServer((req, res) => app.handleRequest(req, res)).listen(8000);
+
+
+import express , {type Request,type Response} from "express";
+
+const app = express();
+
+app.get("/",(req:Request,res:Response)=>{
+  res.send("Hello from server");
+})
+
+app.listen(8000,()=>{console.log("Server started")})
